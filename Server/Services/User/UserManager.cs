92415bca591059b5414095806fb0b8111c8a39eb
Lukas Reschke <lukas@statuscode.ch>
2020-11-14 18:04:03 +0000

    Add method to return all users

--- Server/Services/User/UserManager.cs
+++ Server/Services/User/UserManager.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading;
-using System.Threading.Tasks;
 using AuthServer.Server.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
@@ -13,6 +10,8 @@ namespace AuthServer.Server.Services.User
 {
     public class UserManager : UserManager<AppUser>
     {
+        private readonly AuthDbContext _authDbContext;
+
         public UserManager(
             IUserStore<AppUser> store,
             IOptions<IdentityOptions> optionsAccessor,
@@ -22,12 +21,18 @@ namespace AuthServer.Server.Services.User
             ILookupNormalizer keyNormalizer,
             IdentityErrorDescriber errors,
             IServiceProvider services,
-            ILogger<UserManager<AppUser>> logger
+            ILogger<UserManager<AppUser>> logger,
+            AuthDbContext authDbContext
             ) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
         {
-
+            _authDbContext = authDbContext;
         }
 
-        
+        public IEnumerable<AppUser> GetAllUsers()
+        {
+            List<AppUser> users = _authDbContext.Users.ToList();
+
+            return users;
+        }
     }
 }
