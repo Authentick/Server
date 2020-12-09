7ee1084c2be8138b618b9ac473cbd9ce0f8d51ec
Lukas Reschke <lukas@statuscode.ch>
2020-12-09 14:56:47 +0000

    Add UI for using LetsEncrypt

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using AuthServer.Server.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -21,6 +26,36 @@ namespace AuthServer.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel((builderContext, kestrelOptions) =>
+                    {
+                        kestrelOptions.AddServerHeader = false;
+
+                        kestrelOptions.ListenAnyIP(443, listenOptions =>
+                        {
+                            listenOptions.UseHttps(async (stream, clientHelloInfo, state, cancellationToken) =>
+                            {
+                                await Task.Yield();
+
+                                string? snapFolder = Environment.GetEnvironmentVariable("SNAP_USER_DATA");
+                                string targetHostName = stream.TargetHostName.Replace("/", "").Replace("\\", "");
+                                string targetFile = snapFolder + "/" + targetHostName + ".pfx";
+
+                                SslServerAuthenticationOptions options = new SslServerAuthenticationOptions { };
+
+                                if (File.Exists(targetFile))
+                                {
+                                    options.ServerCertificate = X509Certificate.CreateFromCertFile(targetFile);
+                                }
+                                else
+                                {
+                                    // Use default certificate
+                                }
+
+                                return options;
+                            }, state: null);
+                        });
+                        kestrelOptions.ListenAnyIP(80);
+                    });
                 });
     }
 }
