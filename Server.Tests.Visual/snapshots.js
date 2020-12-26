6efead2895edab2201f69eeaaf469ee7948d256b
Lukas Reschke <lukas@statuscode.ch>
2020-12-26 23:39:46 +0000

    Add percy UI tests

---
+++ Server.Tests.Visual/snapshots.js
@@ -0,0 +1,60 @@
+const PercyScript = require('@percy/script');
+const config = { widths: [576, 768, 992, 1200] };
+const mobileOnlyConfig = { widths: [576, 768] };
+
+PercyScript.run(async (page, percySnapshot) => {
+    await page.goto('http://localhost/');
+    // ensure the page has loaded before capturing a snapshot
+    await page.waitForSelector('.auth-base-root');
+
+    await testInstaller(page, percySnapshot);
+    await testLogin(page, percySnapshot);
+});
+
+async function testInstaller(page, percySnapshot) {
+    var nextButton = 'button:nth-of-type(2)';
+    var previousButton = 'button:nth-of-type(1)';
+
+    await percySnapshot('installer-entry-step', config);
+    await page.click(nextButton);
+    await percySnapshot('installer-tls-choice-step', config);
+    await page.click('#useLetsEncrypt');
+    await page.click(nextButton);
+    await percySnapshot('installer-letsencrypt-step', config);
+    await page.click(previousButton);
+    await percySnapshot('installer-tls-choice-step-after-back', config);
+    await page.click('#useGatekeeperCertificate');
+    await page.click(nextButton);
+    await percySnapshot('installer-email-choice-step', config);
+    await page.click('#useCustomSmtp');
+    await page.click(nextButton);
+    await page.type('input:nth-of-type(1)', 'localhost', {delay: 100});
+    await page.type('input:nth-of-type(2)', '25', {delay: 100});
+    await page.type('input:nth-of-type(3)', 'test', {delay: 100});
+    await page.type('input:nth-of-type(4)', 'test', {delay: 100});
+    await page.type('input:nth-of-type(5)', 'test@example.com', {delay: 100});
+    await page.click(nextButton);
+    await percySnapshot('installer-create-account-step', config);
+    await page.type('input:nth-of-type(1)', 'testuser', {delay: 100});
+    await page.type('input:nth-of-type(2)', 'ins3cureTestUserPassw0rd!', {delay: 100});
+    await page.type('input:nth-of-type(3)', 'test@example.com', {delay: 100});
+    await page.click(nextButton);
+}
+
+async function testLogin(page, percySnapshot) {
+    await percySnapshot('login', config);
+
+    // Pre-Login
+    await page.type('input[type="text"]', 'testuser', {delay: 100});
+    await page.type('input[type="password"]', 'ins3cureTestUserPassw0rd!', {delay: 100});
+    await page.waitForTimeout(1000);
+    await percySnapshot('pre-login', config);
+
+    // Post-Login
+    await page.click('button');
+    await page.waitForTimeout(1000);
+    await percySnapshot('post-login', config);    
+    await page.click('.sidebar__second a');
+    await percySnapshot('mobile-pressed-on-all-apps', mobileOnlyConfig);
+}
+
