6b1601b9a46aa30149c12d3e53659d03f956b05d
Lukas Reschke <lukas@statuscode.ch>
2020-12-27 11:47:59 +0000

    Add screenshots for the security tab

--- Server.Tests.Visual/snapshots.js
+++ Server.Tests.Visual/snapshots.js
@@ -4,13 +4,13 @@ const mobileOnlyConfig = { widths: [576] };
 
 PercyScript.run(async (page, percySnapshot) => {
     page
-    .on('console', message =>
-      console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
-    .on('pageerror', ({ message }) => console.log(message))
-    .on('response', response =>
-      console.log(`${response.status()} ${response.url()}`))
-    .on('requestfailed', request =>
-      console.log(`${request.failure().errorText} ${request.url()}`))
+        .on('console', message =>
+            console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
+        .on('pageerror', ({ message }) => console.log(message))
+        .on('response', response =>
+            console.log(`${response.status()} ${response.url()}`))
+        .on('requestfailed', request =>
+            console.log(`${request.failure().errorText} ${request.url()}`))
 
     await page.goto('http://localhost/');
     // ensure the page has loaded before capturing a snapshot
@@ -18,6 +18,7 @@ PercyScript.run(async (page, percySnapshot) => {
 
     await testInstaller(page, percySnapshot);
     await testLogin(page, percySnapshot);
+    await testSecurity(page, percySnapshot);
 });
 
 async function testInstaller(page, percySnapshot) {
@@ -37,17 +38,17 @@ async function testInstaller(page, percySnapshot) {
     await percySnapshot('installer-email-choice-step', config);
     await page.click('#useCustomSmtp');
     await page.click(nextButton);
-    await page.type('input:nth-of-type(1)', 'localhost', {delay: 100});
-    await page.type('input:nth-of-type(2)', '25', {delay: 100});
-    await page.type('input:nth-of-type(3)', 'test', {delay: 100});
-    await page.type('input:nth-of-type(4)', 'test', {delay: 100});
-    await page.type('input:nth-of-type(5)', 'test@example.com', {delay: 100});
+    await page.type('input:nth-of-type(1)', 'localhost', { delay: 100 });
+    await page.type('input:nth-of-type(2)', '25', { delay: 100 });
+    await page.type('input:nth-of-type(3)', 'test', { delay: 100 });
+    await page.type('input:nth-of-type(4)', 'test', { delay: 100 });
+    await page.type('input:nth-of-type(5)', 'test@example.com', { delay: 100 });
     await page.click('body');
     await page.click(nextButton);
     await percySnapshot('installer-create-account-step', config);
-    await page.type('input:nth-of-type(1)', 'testuser', {delay: 100});
-    await page.type('input:nth-of-type(2)', 'ins3cureTestUserPassw0rd!', {delay: 100});
-    await page.type('input:nth-of-type(3)', 'test@example.com', {delay: 100});
+    await page.type('input:nth-of-type(1)', 'testuser', { delay: 100 });
+    await page.type('input:nth-of-type(2)', 'ins3cureTestUserPassw0rd!', { delay: 100 });
+    await page.type('input:nth-of-type(3)', 'test@example.com', { delay: 100 });
     await page.keyboard.press('Tab');
     await page.click(nextButton);
     await page.waitForTimeout(5000);
@@ -57,16 +58,45 @@ async function testLogin(page, percySnapshot) {
     await percySnapshot('login', config);
 
     // Pre-Login
-    await page.type('input[type="text"]', 'testuser', {delay: 100});
-    await page.type('input[type="password"]', 'ins3cureTestUserPassw0rd!', {delay: 100});
+    await page.type('input[type="text"]', 'testuser', { delay: 100 });
+    await page.type('input[type="password"]', 'ins3cureTestUserPassw0rd!', { delay: 100 });
     await page.waitForTimeout(1000);
     await percySnapshot('pre-login', config);
 
     // Post-Login
     await page.click('button');
     await page.waitForTimeout(1000);
-    await percySnapshot('post-login', config);    
+    await percySnapshot('post-login', config);
     await page.click('.sidebar__second a');
     await percySnapshot('mobile-pressed-on-all-apps', mobileOnlyConfig);
 }
 
+async function testSecurity(page, percySnapshot) {
+    await clickPrimaryNav(page, 2);
+    await percySnapshot('security-mobile-nav', mobileOnlyConfig);
+
+    // Security overview page
+    await clickSeconaryNav(page, 1);
+    await percySnapshot('security-settings-index', config);
+
+    // Sessions page
+    await clickPrimaryNav(page, 2);
+    await clickSeconaryNav(page, 3);
+    await percySnapshot('security-alerts', config);
+}
+
+/**
+ * @param {*} page 
+ * @param {int} number 
+ */
+async function clickSeconaryNav(page, number) {
+    await page.click('.sidebar__second a:nth-of-type(' + number + ')');
+}
+
+/**
+ * @param {*} page 
+ * @param {int} number 
+ */
+async function clickPrimaryNav(page, number) {
+    await page.click('.sidebar__icon:nth-of-type(' + number + ')');
+}
