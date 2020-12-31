9b886d72359f8fb9627eecab0ece353336d8aff7
Lukas Reschke <lukas@statuscode.ch>
2020-12-31 21:07:48 +0000

    Add NLog

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
-using AuthServer.Server.Models;
 using AuthServer.Server.Services.TLS;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Https;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace AuthServer.Server
 {
@@ -19,7 +14,32 @@ namespace AuthServer.Server
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            string logFileLocation = AuthServer.Server.Services.FileStorage.PathProvider.GetApplicationDataFolder() + "/gatekeeper-logs.txt";
+
+            NLog.Config.LoggingConfiguration config = new NLog.Config.LoggingConfiguration();
+            NLog.Targets.FileTarget logFileTarget = new NLog.Targets.FileTarget("logfile") { FileName = logFileLocation };
+            NLog.Targets.ConsoleTarget logConsoleTarget = new NLog.Targets.ConsoleTarget("logconsole");
+            config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logConsoleTarget);
+            config.AddRule(NLog.LogLevel.Error, NLog.LogLevel.Fatal, logFileTarget);
+
+            NLog.Logger logger = NLogBuilder.ConfigureNLog(config).GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -31,28 +51,34 @@ namespace AuthServer.Server
                     {
                         kestrelOptions.AddServerHeader = false;
 
-                     kestrelOptions.ListenAnyIP(443, listenOptions =>
-                        {
-                            listenOptions.UseHttps(async (stream, clientHelloInfo, state, cancellationToken) =>
-                            {
-                                await Task.Yield();
+                        kestrelOptions.ListenAnyIP(443, listenOptions =>
+                           {
+                               listenOptions.UseHttps(async (stream, clientHelloInfo, state, cancellationToken) =>
+                               {
+                                   await Task.Yield();
 
-                                SslServerAuthenticationOptions options = new SslServerAuthenticationOptions { };
+                                   SslServerAuthenticationOptions options = new SslServerAuthenticationOptions { };
 
-                                if (CertificateLocationHelper.CertificateExists(clientHelloInfo.ServerName))
-                                {
-                                    options.ServerCertificate = new X509Certificate2(CertificateLocationHelper.GetPath(clientHelloInfo.ServerName));
-                                }
-                                else
-                                {
+                                   if (CertificateLocationHelper.CertificateExists(clientHelloInfo.ServerName))
+                                   {
+                                       options.ServerCertificate = new X509Certificate2(CertificateLocationHelper.GetPath(clientHelloInfo.ServerName));
+                                   }
+                                   else
+                                   {
                                     // FIXME: Use default certificate
                                 }
 
-                                return options;
-                            }, state: null);
-                        });
+                                   return options;
+                               }, state: null);
+                           });
                         kestrelOptions.ListenAnyIP(80);
                     });
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
---
+++ Server/Services/FileStorage/PathProvider.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace AuthServer.Server.Services.FileStorage
+{
+    static class PathProvider
+    {
+        public static string GetApplicationDataFolder()
+        {
+            string? snapFolder = Environment.GetEnvironmentVariable("SNAP_USER_DATA");
+            if (snapFolder == null)
+            {
+                // FIXME: This is for development installations and not really great. This should not be the temp folder but 
+                // something more dedicated.
+                snapFolder = "/tmp";
+            }
+
+            return snapFolder;
+        }
+    }
+}
