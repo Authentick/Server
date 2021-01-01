bd007c0870af49a1c722be3709090f2aaea07e36
Lukas Reschke <lukas@statuscode.ch>
2021-01-01 13:45:06 +0000

    Add new field to store public endpoints

---
+++ Server/Migrations/20210101133541_AddEndpointsWithoutAuth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthServer.Server.Migrations
+{
+    public partial class AddEndpointsWithoutAuth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "EndpointsWithoutAuth",
+                table: "ProxyAppSettings",
+                type: "text[]",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndpointsWithoutAuth",
+                table: "ProxyAppSettings");
+        }
+    }
+}
