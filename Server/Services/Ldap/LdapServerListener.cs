12395d0369cac2a46190b6ad39eeaefc97bf373b
Lukas Reschke <lukas@statuscode.ch>
2021-01-02 19:44:47 +0000

    Add ILogger

--- Server/Services/Ldap/LdapServerListener.cs
+++ Server/Services/Ldap/LdapServerListener.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Gatekeeper.LdapServerLibrary;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AuthServer.Server.Services.Ldap
 {
     class LdapServerListener : BackgroundService
     {
         private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly ILogger<LdapServerListener> _logger;
 
-        public LdapServerListener(IServiceScopeFactory serviceScopeFactory)
+        public LdapServerListener(
+            IServiceScopeFactory serviceScopeFactory,
+            ILogger<LdapServerListener> logger)
         {
             _serviceScopeFactory = serviceScopeFactory;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -26,7 +32,23 @@ namespace AuthServer.Server.Services.Ldap
 
                 LdapEventListener ldapEventListener = scope.ServiceProvider.GetRequiredService<LdapEventListener>();
                 server.RegisterEventListener(ldapEventListener);
+                server.RegisterLogger(new LdapLogger(_logger));
                 await server.Start();
+            }
+        }
+
+        private class LdapLogger : Gatekeeper.LdapServerLibrary.ILogger
+        {
+            private readonly ILogger<LdapServerListener> _logger;
+
+            public LdapLogger(ILogger<LdapServerListener> logger)
+            {
+                _logger = logger;
+            }
+
+            public void LogException(Exception e)
+            {
+                _logger.LogError(1, e, "Parsing LDAP packet failed");
             }
         }
     }
