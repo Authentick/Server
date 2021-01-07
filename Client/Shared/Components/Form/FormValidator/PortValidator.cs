64aeac11ed71117854c5884922a8df0a964c4574
Lukas Reschke <lukas@statuscode.ch>
2021-01-07 13:37:56 +0000

    Migrate setup to new forms

---
+++ Client/Shared/Components/Form/FormValidator/PortValidator.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuthServer.Client.Shared.Components.Form.FormValidator
+{
+    public class PortValidator : IFormValidator
+    {
+        public Task<FormValidatorResponse> Check(string value, CancellationToken cancellationToken)
+        {
+            try {
+                int port = Int32.Parse(value);
+                if(port >= 1 && port <= 65535)
+                {
+                    return Task.FromResult(new FormValidatorResponse(true, null));
+                }
+            } catch (FormatException) {}
+
+            return Task.FromResult(new FormValidatorResponse(false, "Not a valid port number"));
+        }
+    }
+}
